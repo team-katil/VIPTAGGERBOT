a95ca3974ddfd1367db27eaf1fc8e1989f95eabc
team-katil <115205040+team-katil@users.noreply.github.com>
2023-04-27 13:22:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 import os
 import heroku3
-from telethon import TelegramClient, events
+from telethon import TelegramClient, events ,
 #
 # Here Don't Worry 
 # 
 # 
-api_id = int(os.environ.get("APP_ID", "11660229"))
-api_hash = os.environ.get("API_HASH", "b89dc605daa9a5e957559402cc19856b")
-bot_token = os.environ.get("TOKEN", "6076917136:AAHY2H09i7E-0MRPteUeh9HdJ_0V8Qq3yIs")
+api_id = "11660229"
+api_hash = "b89dc605daa9a5e957559402cc19856b"
+bot_token = "6076917136:AAHY2H09i7E-0MRPteUeh9HdJ_0V8Qq3yIs"
 
 # Telethon 
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 #
-USERNAME = os.environ.get("USERNAME", "KATIL_YOUR_DAD")
-log_group = int(os.environ.get("LOG_GROUP", "-1001827910417"))
-startmessage = os.environ.get("startmessage")
-commands = os.environ.get("commands")
-groupstart = os.environ.get("groupstart")
-support = os.environ.get("support")
-owner = os.environ.get("owner")
+USERNAME = "viptaggerbot")
+log_group = "-1001827910417"
+startmessage = "\n\n• IN YOUR GROUP ALMOST ALL MEMBER I CAN TAG . . . • CLICK ALL COMMANDS BUTTON AND CHECK . . ."
+commands = "♨️ all commands ;\n\n» /utag   <  𝗆𝖾𝗌sage  >\n   - Members of 5 tags .\n\n» /tag   <  𝗆𝖾𝗌sage  >\n   - tag one by one members .\n\n» /atag   <  𝗆𝖾𝗌sage  >\n   - tag only admins .\n\n» /etag   <  𝗆essage  >\n   - tag with emoji .\n\n» /stag   <  𝗆𝖾𝗌sage  >\n   - tag member with special word .\n\n» /cancel =>\n   - cancel tagging process ."
+groupstart = "• Active now I am working . . .\n\n• commands To see For your bot Inside start it login . . ."
+support = "katilsupport"
+owner = "katil_your_dad"
