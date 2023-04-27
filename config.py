3d4e982e7314e9362013df90db02c237af15d33a
team-katil <115205040+team-katil@users.noreply.github.com>
2023-04-27 17:48:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,9 +13,9 @@ bot_token = "6076917136:AAHY2H09i7E-0MRPteUeh9HdJ_0V8Qq3yIs"
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 #
 USERNAME = "viptaggerbot"
-log_qrup = -1001827910417
-startmesaj = "\n\n• IN YOUR GROUP ALMOST ALL MEMBER I CAN TAG . . . • CLICK ALL COMMANDS BUTTON AND CHECK . . ."
-komutlar = "♨️ all commands ;\n\n» /utag   <  𝗆𝖾𝗌sage  >\n   - Members of 5 tags .\n\n» /tag   <  𝗆𝖾𝗌sage  >\n   - tag one by one members .\n\n» /atag   <  𝗆𝖾𝗌sage  >\n   - tag only admins .\n\n» /etag   <  𝗆essage  >\n   - tag with emoji .\n\n» /stag   <  𝗆𝖾𝗌sage  >\n   - tag member with special word .\n\n» /cancel =>\n   - cancel tagging process ."
-qrupstart = "• Active now I am working . . .\n\n• commands To see For your bot Inside start it login . . ."
+log_group = -1001827910417
+startmessage = "\n\n• IN YOUR GROUP ALMOST ALL MEMBER I CAN TAG . . . • CLICK ALL COMMANDS BUTTON AND CHECK . . ."
+commands = "♨️ all commands ;\n\n» /utag   <  𝗆𝖾𝗌sage  >\n   - Members of 5 tags .\n\n» /tag   <  𝗆𝖾𝗌sage  >\n   - tag one by one members .\n\n» /atag   <  𝗆𝖾𝗌sage  >\n   - tag only admins .\n\n» /etag   <  𝗆essage  >\n   - tag with emoji .\n\n» /stag   <  𝗆𝖾𝗌sage  >\n   - tag member with special word .\n\n» /cancel =>\n   - cancel tagging process ."
+groupstart = "• Active now I am working . . .\n\n• commands To see For your bot Inside start it login . . ."
 support = "katilsupport"
-sahib = "katil_your_dad"
+owner = "katil_your_dad"
