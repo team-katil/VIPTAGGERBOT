4d44df6996517f3d9057039aadbe0b58274f17e0
team-katil <115205040+team-katil@users.noreply.github.com>
2023-04-27 14:35:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ bot_token = "6076917136:AAHY2H09i7E-0MRPteUeh9HdJ_0V8Qq3yIs"
 # Telethon 
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 #
-USERNAME = "viptaggerbot")
+USERNAME = "viptaggerbot"
 log_group = "-1001827910417"
 startmessage = "\n\n• IN YOUR GROUP ALMOST ALL MEMBER I CAN TAG . . . • CLICK ALL COMMANDS BUTTON AND CHECK . . ."
 commands = "♨️ all commands ;\n\n» /utag   <  𝗆𝖾𝗌sage  >\n   - Members of 5 tags .\n\n» /tag   <  𝗆𝖾𝗌sage  >\n   - tag one by one members .\n\n» /atag   <  𝗆𝖾𝗌sage  >\n   - tag only admins .\n\n» /etag   <  𝗆essage  >\n   - tag with emoji .\n\n» /stag   <  𝗆𝖾𝗌sage  >\n   - tag member with special word .\n\n» /cancel =>\n   - cancel tagging process ."
