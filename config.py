c8153d14035b347bbfc39fca215b4aa0431678bc
team-katil <115205040+team-katil@users.noreply.github.com>
2023-04-27 09:12:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ from telethon import TelegramClient, events
 # Here Don't Worry 
 # 
 # 
-api_id = int(os.environ.get("APP_ID", "9316256"))
-api_hash = os.environ.get("API_HASH", "5a8a277605c3038129c536a9e79cd761")
-bot_token = os.environ.get("TOKEN", "5864136148:AAGTTxKCYQm-ykN9AbEikY05M8qPcUTNiTI")
+api_id = int(os.environ.get("APP_ID", "11660229"))
+api_hash = os.environ.get("API_HASH", "b89dc605daa9a5e957559402cc19856b")
+bot_token = os.environ.get("TOKEN", "6076917136:AAHY2H09i7E-0MRPteUeh9HdJ_0V8Qq3yIs")
 
 # Telethon 
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
